1099952d1b0f73995a824104502459836559bc3e
Leto <leto@kauler.com>
2013-08-11 00:16:07 +1000

    Tromb: Post a new person to server

--- FxOS/people.js
+++ FxOS/people.js
@@ -1,25 +1,54 @@
 
 var tromb = angular.module('tromb', []);
 
-tromb.controller('PeopleCtrl', function($scope) {
+tromb.controller('PeopleCtrl', function($scope, $http) {
+	//$http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
+
 	$scope.people = [
-		{ name: 'Bob', occupation: 'Web dev', company: 'Comp1', twitterHandle: 'Bob111', photo: "blob:b21ed2f5-e600-414a-91c1-53f68169f031" },
-		{ name: 'John', occupation: 'Web dev', company: 'Comp2', twitterHandle: 'JohnXXX', photo: "blob:eb1b20d8-c2fa-2e46-9e50-7f143bb362c6" }
+		{ name: 'Bob', occupation: 'Web dev', company: 'Comp1', twitter: 'Bob111', photo: "blob:b21ed2f5-e600-414a-91c1-53f68169f031" },
+		{ name: 'John', occupation: 'Web dev', company: 'Comp2', twitter: 'JohnXXX', photo: "blob:eb1b20d8-c2fa-2e46-9e50-7f143bb362c6" }
 	];
 
 	$scope.addPerson = function() {
-		$scope.addPersonComplete();
-		return;
+		//$scope.addPersonComplete();
+		//return;
 
+		var obj = {
+			name: $scope.name,
+			occupation: $scope.occupation,
+			company: $scope.company,
+			twitter: $scope.twitter,
+			photo: $scope.photo,
+			photoBlob: $scope.photoBlob
+		};
+
+		console.log(obj);
+
+		$http({
+			method: 'POST',
+			url: 'http://192.168.88.203:3000/trombine/new',
+			//headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+			data: obj
+		})
+		.success(function(data, status) {
+			console.log('post success');
+			$scope.addPersonComplete(obj);
+		})
+		.error(function(data, status) {
+			console.log('post error');
+			alert('Error: ' + status + ' ' + data);
+		});
+
+/*
 		var form = new FormData();
 		form.append('name', $scope.name);
 		form.append('occupation', $scope.occupation);
 		form.append('company', $scope.company);
-		form.append('twitter', $scope.twitterHandle);
+		form.append('twitter', $scope.twitter);
 		form.append('photoBlob',  $scope.photoBlob);
 
 		var xhr = new XMLHttpRequest();
-		xhr.open('POST', 'getfile.php');
+		xhr.open('POST', 'http://192.168.88.203:3000/trombine/new');
 		xhr.send(form);
 
 		xhr.onreadystatechange = function () {
@@ -28,27 +57,21 @@ tromb.controller('PeopleCtrl', function($scope) {
 					$scope.addPersonComplete();
 				}
 				else {
-					alert('Failed...');
+					alert('Failed ' + xhr.status + ' ' + xhr.responseText);
 				}
 			}
-		};
+		};*/
 	};
 
-	$scope.addPersonComplete = function() {
+	$scope.addPersonComplete = function(obj) {
 		// Add person to people collection
-		$scope.people.push({
-			name: $scope.name,
-			occupation: $scope.occupation,
-			company: $scope.company,
-			twitterHandle: $scope.twitterHandle,
-			photo: $scope.photo
-		});
+		$scope.people.push(obj);
 
 		//Clear form
 		$scope.name = '';
 		$scope.occupation = '';
 		$scope.company = '';
-		$scope.twitterHandle = '';
+		$scope.twitter = '';
 		$scope.photo = '';
 		$scope.photoBlob = '';
 		//$scope.$setPristine();
