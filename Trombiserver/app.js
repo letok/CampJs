99e5c2bc1c5dc46261cbaf38b1e6e4af8655a69f
Guillaume <niouton@gmail.com>
2013-08-10 22:34:34 +1000

    First version of the Trombiserver.

---
+++ Trombiserver/app.js
@@ -0,0 +1,71 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path')
+  , Trombine = require('./trombine').Trombine;
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.cookieParser('My wonderful secret'));
+app.use(express.session());
+app.use(app.router);
+app.use(require('less-middleware')({ src: __dirname + '/public' }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+var trombine= new Trombine('localhost', 27017);
+
+// Routes
+app.get('/', function(req, res){
+  
+  trombine.save({
+    name: "My test name"
+  }, function( error, docs) {});
+
+  /*
+  trombine.remove(function(error){
+  	res.render('error');
+  });
+  */
+
+  trombine.findAll(function(error, trombines){
+      res.render('index', {
+            title: 'Trombines',
+            trombines: trombines
+        });
+  });
+});
+
+app.post('/trombine/new', function(req, res){
+	trombine.save({
+        name: req.param('name'),
+        occupation: req.param('occupation'),
+        company: req.param('company'),
+        twitter: req.param('twitter')/*,
+        photoBlob: req.param('photoBlob')*/
+    }, function( error, docs) {
+        res.redirect('/')
+    });
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ Trombiserver/trombine.js
@@ -0,0 +1,63 @@
+var Db = require('mongodb').Db;
+var Connection = require('mongodb').Connection;
+var Server = require('mongodb').Server;
+var BSON = require('mongodb').BSON;
+var ObjectID = require('mongodb').ObjectID;
+
+Trombine = function(host, port) {
+  this.db= new Db('trombinoscope', new Server(host, port, {safe: false}, {auto_reconnect: true}, {}));
+  this.db.open(function(){});
+};
+
+
+Trombine.prototype.getCollection= function(callback) {
+  this.db.collection('trombines', function(error, trombine_collection) {
+    if( error ) callback(error);
+    else callback(null, trombine_collection);
+  });
+};
+
+//find all trombines
+Trombine.prototype.findAll = function(callback) {
+    this.getCollection(function(error, trombine_collection) {
+      if( error ) callback(error)
+      else {
+        trombine_collection.find().toArray(function(error, results) {
+          if( error ) callback(error)
+          else callback(null, results)
+        });
+      }
+    });
+};
+
+//remove all
+Trombine.prototype.remove = function(callback) {
+    this.getCollection(function(error, trombine_collection) {
+      if( error ) callback(error)
+      else {
+        trombine_collection.remove({name: "My test name"});
+      }
+    });  
+};
+
+//save new trombine
+Trombine.prototype.save = function(trombines, callback) {
+    this.getCollection(function(error, trombine_collection) {
+      if( error ) callback(error)
+      else {
+        if( typeof(trombines.length)=="undefined")
+          trombines = [trombines];
+
+        for( var i =0;i< trombines.length;i++ ) {
+          trombine = trombines[i];
+          trombine.created_at = new Date();
+        }
+
+        trombine_collection.insert(trombines, function() {
+          callback(null, trombines);
+        });
+      }
+    });
+};
+
+exports.Trombine = Trombine;
